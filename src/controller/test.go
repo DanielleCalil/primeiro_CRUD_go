426d075dfe26b848e509e09fc15f9b687dca9dbe
Daniicalil <dacalil90@gmail.com>
2025-01-22 14:26:43 -0300

    up

---
+++ src/controller/test.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/primeiro_CRUD_go/src/configuration/rest_err"
+
+func test(message string) (err *rest_err.RestErr) {
+
+	err.Code
+
+}
---
+++ src/controller/updatedUser.go
@@ -0,0 +1,5 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func UpdatedUser(c *gin.Context) {}
